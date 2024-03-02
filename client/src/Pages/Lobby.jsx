30da395cab2ca597ada2fb727ec06e67ac015f0a
imdinnesh <121331053+imdinnesh@users.noreply.github.com>
2024-03-02 13:17:55 +0530

    Minor changes

--- client/src/Pages/Lobby.jsx
+++ client/src/Pages/Lobby.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../Context/SocketProvider";
+import '../App.css'
 
 const LobbyScreen = () => {
   const [email, setEmail] = useState("");
@@ -34,26 +35,42 @@ const LobbyScreen = () => {
 
   return (
     <div>
-      <h1>Lobby</h1>
-      <form onSubmit={handleSubmitForm}>
-        <label htmlFor="email">Email ID</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <label htmlFor="room">Room Number</label>
-        <input
-          type="text"
-          id="room"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <br />
-        <button>Join</button>
-      </form>
+      <div className="flex">
+        
+
+        <div className="w-3/4 h-screen">
+          <div className="rounded-lg shadow-lg bg-white m-auto mt-40 p-8 w-2/5">
+
+            <form className='flex flex-col items-center justify-center space-y-1.5'
+              onSubmit={handleSubmitForm}>
+              <div className='mt-10'>
+
+                <label htmlFor='email' className='w-24 h-10 inline-block'>Enter Email</label>
+                <input type="email" id='email'
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  className='w-64 h-10 border-2 px-1 border-gray-300 rounded-md' />
+              </div>
+              <div>
+
+                <label htmlFor='room' className='w-24 h-10 inline-block'>Enter Room</label>
+                <input type="text" id='room'
+                  value={room}
+                  onChange={(event) => setRoom(event.target.value)}
+                  className='w-64 h-10 border-2 px-1  border-gray-300 rounded-md' />
+              </div>
+
+              <button
+                className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'>
+                Enter Room
+              </button>
+            </form>
+          </div>
+
+        </div>
+
+        <div className="w-1/4 h-screen bg-slate-500"></div>
+      </div>
     </div>
   );
 };
